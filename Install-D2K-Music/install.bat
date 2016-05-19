4f78bfb5aef4390376b70fb2967fa14dc99ff68b
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2016-05-19 04:05:33 +0200

    added D2K script to install music automatically

---
+++ Install-D2K-Music/install.bat
@@ -0,0 +1,2 @@
+mkdir "%UserProfile%\Documents\OpenRA\Content\d2k\Music"
+unzip ..\d2k-music.zip -d "%UserProfile%\Documents\OpenRA\Content\d2k"
---
+++ Install-D2K-Music/install.sh
@@ -0,0 +1,2 @@
+mkdir -p "~/.openra/Content/d2k/Music"
+unzip "../d2k-music.zip" -d "~/.openra/Content/d2k"
