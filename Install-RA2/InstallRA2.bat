8a8e8208fc89908005688ec717b9b75f8c817a53
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2016-05-27 01:38:22 +0200

    Updated to latest working OpenRA and ra2 version

--- Install-RA2/InstallRA2.bat
+++ Install-RA2/InstallRA2.bat
@@ -5,7 +5,7 @@ REM download & build OpenRA
     
     REM  git checkout master
     REM master branch crashes as the mod is loading
-    git checkout b4664d1ec59f37714522268caf55203e96c7fb19
+    git checkout b6afc765b4be0b0289471c25c450d79eb948d3d3
     REM checkout latest commit at time of writing so future builds use the same version
     
     cmd /c make dependencies
@@ -18,7 +18,7 @@ REM download & build RA2
     git clone https://github.com/OpenRA/ra2
     cd ra2
     
-    git checkout 6544d26c4a38d090b75d37f6d190b64b5c9e258c
+    git checkout a1b6dd03109e00515cff9658a91dbe09d9fde0dd
     REM checkout latest commit at time of writing so future builds use the same version
     
     cmd /c make dependencies "%~dp0OpenRA"
