6cb1fbb1f756d0c0a37bd5a5634013383364935f
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2016-06-07 14:58:36 +0200

    Fixed up linux scripts (actually tested them)
    - made all linux scripts executable
    - use `unrar` instead of `7z` for cnc script, `7z` fails with "Unsupported Method"
    - simplified d2k movies script a lot

--- Install-Movies/install-cnc.sh
+++ Install-Movies/install-cnc.sh
@@ -2,4 +2,4 @@ mkdir -p "$HOME/.openra/Content/cnc"
 if [ ! -e cnc1_movies_full.rar ]; then
     wget "http://download.digiex.net/Games/C&C Gold/cnc1_movies_full.rar"
 fi
-7z e cnc1_movies_full.rar -o"$HOME/.openra/Content/cnc"
+unrar e cnc1_movies_full.rar "$HOME/.openra/Content/cnc"
--- Install-Movies/install-d2k.sh
+++ Install-Movies/install-d2k.sh
@@ -1,3 +1,6 @@
-mkdir -p "$HOME/.openra/Content/d2k/Movies"
-7z e Dune_2000_Gruntmods_Edition_Cutscenes.exe -o"$HOME/.openra/Content/d2k/Movies"
-rm "$HOME/.openra/Content/d2k/Movies/*.dll" "$HOME/.openra/Content/d2k/Movies/*.ini" "$HOME/.openra/Content/d2k/Movies/*.bmp"
+D2kMoviesPath=$HOME/.openra/Content/d2k/Movies
+mkdir -p "$D2kMoviesPath"
+7z e Dune_2000_Gruntmods_Edition_Cutscenes.exe -o"$D2kMoviesPath"
+pushd $D2kMoviesPath
+    rm *.dll *.ini *.bmp
+popd
