a306e1d41d19031cfe4ddc322a017dcf5610e689
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2016-06-13 02:06:13 +0200

    Allow paths with spaces

--- Install-RA2/install-ra2.sh
+++ Install-RA2/install-ra2.sh
@@ -21,14 +21,14 @@
     git checkout 549eec75f9a6d862fe150431c98abfc78f73c265
     # checkout latest commit at time of writing so future builds use the same version
 
-    OpenRADir=$(dirname $(pwd))/OpenRA
+    OpenRADir=$(dirname "$(pwd)")/OpenRA
     # Following https://github.com/OpenRA/ra2/wiki/Step-by-Step#unix
-    ln -s ../ra2/OpenRA.Mods.RA2 $OpenRADir/OpenRA.Mods.RA2
-    ln -s $OpenRADir/Eluant.dll $OpenRADir/OpenRA.Mods.RA2/dependencies/Eluant.dll
-    ln -s $OpenRADir/OpenRA.Game.exe $OpenRADir/OpenRA.Mods.RA2/dependencies/OpenRA.Game.exe
-    ln -s $OpenRADir/mods/common/OpenRA.Mods.Common.dll $OpenRADir/OpenRA.Mods.RA2/dependencies/OpenRA.Mods.Common.dll
-    ln -s $OpenRADir/mods/ra/OpenRA.Mods.RA.dll $OpenRADir/OpenRA.Mods.RA2/dependencies/OpenRA.Mods.RA.dll
-    ln -s $OpenRADir/mods/ts/OpenRA.Mods.TS.dll $OpenRADir/OpenRA.Mods.RA2/dependencies/OpenRA.Mods.TS.dll
+    ln -s ../ra2/OpenRA.Mods.RA2 "$OpenRADir/OpenRA.Mods.RA2"
+    ln -s "$OpenRADir/Eluant.dll" "$OpenRADir/OpenRA.Mods.RA2/dependencies/Eluant.dll"
+    ln -s "$OpenRADir/OpenRA.Game.exe" "$OpenRADir/OpenRA.Mods.RA2/dependencies/OpenRA.Game.exe"
+    ln -s "$OpenRADir/mods/common/OpenRA.Mods.Common.dll" "$OpenRADir/OpenRA.Mods.RA2/dependencies/OpenRA.Mods.Common.dll"
+    ln -s "$OpenRADir/mods/ra/OpenRA.Mods.RA.dll" "$OpenRADir/OpenRA.Mods.RA2/dependencies/OpenRA.Mods.RA.dll"
+    ln -s "$OpenRADir/mods/ts/OpenRA.Mods.TS.dll" "$OpenRADir/OpenRA.Mods.RA2/dependencies/OpenRA.Mods.TS.dll"
 
     cd OpenRA.Mods.RA2
     xbuild
@@ -36,20 +36,20 @@
 
 # build done, now copy files
     OutDir=$OpenRADir/mods/ra2
-    mkdir -p $OutDir
-    cp -r audio $OutDir/audio
-    cp -r bits $OutDir/bits
-    cp -r chrome $OutDir/chrome
-    cp -r languages $OutDir/languages
-    cp -r maps $OutDir/maps
-    cp -r rules $OutDir/rules
-    cp -r sequences $OutDir/sequences
-    cp -r tilesets $OutDir/tilesets
-    cp -r uibits $OutDir/uibits
-    cp -r weapons $OutDir/weapons
-    cp chrome.yaml cursors.yaml logo.png metrics.yaml mod.yaml preview.png weapons.yaml OpenRA.Mods.RA2.dll $OutDir
+    mkdir -p "$OutDir"
+    cp -r audio "$OutDir/audio"
+    cp -r bits "$OutDir/bits"
+    cp -r chrome "$OutDir/chrome"
+    cp -r languages "$OutDir/languages"
+    cp -r maps "$OutDir/maps"
+    cp -r rules "$OutDir/rules"
+    cp -r sequences "$OutDir/sequences"
+    cp -r tilesets "$OutDir/tilesets"
+    cp -r uibits "$OutDir/uibits"
+    cp -r weapons "$OutDir/weapons"
+    cp chrome.yaml cursors.yaml logo.png metrics.yaml mod.yaml preview.png weapons.yaml OpenRA.Mods.RA2.dll "$OutDir"
 
 rm -r "../OpenRA/mods/cnc"
 
-echo "To play with the RA2 mod, use: $OpenRADir/launch-game.sh"
+echo "To play with the RA2 mod, use: \"$OpenRADir/launch-game.sh\""
 # xdg-open $(dirname $(pwd))/OpenRA
