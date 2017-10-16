565b183237dda9c6666171167a28378e470bc6b1
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2017-10-16 02:02:44 +0200

    Fixed Windows D2K music install script

--- Install-D2K-Music/install.bat
+++ Install-D2K-Music/install.bat
@@ -1,2 +1,2 @@
-mkdir "%UserProfile%\Documents\OpenRA\Content\d2k"
-..\utilities\unzip ..\d2k-music.zip -d "%UserProfile%\Documents\OpenRA\Content\d2k"
+mkdir "%UserProfile%\Documents\OpenRA\Content\d2k\v2"
+..\utilities\unzip ..\d2k-music.zip -d "%UserProfile%\Documents\OpenRA\Content\d2k\v2"
